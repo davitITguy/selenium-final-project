1780c3618bfa9352466d1d8b2b2c0a27b73bf744
= <37460419+davitITguy@users.noreply.github.com>
2021-08-09 03:48:11 +0400

    fleat: fixed WebTablesTest

--- src/test/java/WebTablesTest.java
+++ src/test/java/WebTablesTest.java
@@ -1,39 +1,24 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import net.bytebuddy.pool.TypePool;
-import org.jsoup.select.Evaluator;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 
 public class WebTablesTest {
 
-    public class Tables {
-        WebDriver driver;
+    @Test
+    public void WebTablesTest (){
 
-        public Tables() {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        }
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    String url = "http://techcanvass.com/Examples/webtable.html";
+    driver.get(url);
 
-        @Test
-        public void handleStaticTable() {
-            driver.get("http://techcanvass.com/Examples/webtable.html");
-            driver.manage().window().maximize();
-            System.out.println(driver.findElement(By.xpath("//*[@id='t01']/tbody/tr[1]/th[1]")).getText());
+    WebElement CarBrand = driver.findElement(By.xpath("//table//tr[last()]//td"));
+    System.out.println(CarBrand.getText());
+    WebElement CarPrice = driver.findElement(By.xpath("//table//tr[last()]//td[last()]"));
+    System.out.println(CarPrice.getText());
 
-            for (int rowno = 1; rowno <= 4; rowno++) {
-                for (int colno = 1; colno <= 4; colno++) {
-                    System.out.println(driver.findElement(By.xpath("//*[@id='t01']/tbody/tr[" + rowno + "]/td[" + colno + "]")).getText());
-
-                }
-            }
-        }
     }
 }
+
